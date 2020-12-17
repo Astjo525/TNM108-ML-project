5834e51ad74d67ef81783a5c6b606e6f4b03943b
Loual201 <louise.almer97@gmail.com>
2020-12-17 11:35:21 +0100

    la till lite kommentarer

--- scrape_reddit.py
+++ scrape_reddit.py
@@ -8,14 +8,17 @@ from variables import *
 
 #exec(open('scrape_reddit.py').read())
 
+#Using praw to scrape Reddit, with our app
 reddit = praw.Reddit(client_id=client_id, \
                      client_secret=client_secret, \
                      user_agent=user_agent, \
                      username=username, \
                      password=password)
 
+#scrape the 100 first questions 
 numb_questions = 100
 
+#Scrape the subreddit AskHistorians with the 100 top questions of all time.
 top_subreddit = reddit.subreddit('AskHistorians').top("all", limit=numb_questions)
 topics_dict = { "title":[],
                 "replies":[] }
