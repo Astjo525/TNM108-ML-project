6119f973ec57ead76af7448ebbdcf3650a40eac7
Loual201 <louise.almer97@gmail.com>
2020-12-17 11:30:58 +0100

    la till kommentarer

--- tfidf.py
+++ tfidf.py
@@ -10,15 +10,19 @@ def minimum_cosine(query, data):
     data = data.copy()
     data.append(query)
 
+    #Transform all the answers and questions into tfidf vectors
     TfidfVec = TfidfVectorizer()
     tfidf = TfidfVec.fit_transform(data)
 
+    #Store all the cosine angles in theta
     theta = []
+    #Compute the cosine similarity between the query and the data.
     for i in range(len(data) - 1):
         val = cosine_similarity(tfidf[-1], tfidf[i])
 
         theta.append(math.acos(val))
-
+    
+    #find the minimum angle and the index of that text from data and return it
     min_angle = min(theta)
     data_index = theta.index(min_angle)
 
